b6e9d5ca43cf7b95e946431ec48b007c08a36a52
李石磊 <1156021653@qq.com>
2018-08-23 13:37:56 +0800

    feat: style

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const querystring = require('querystring');
 const fs = require('fs');
 const nodePath = require('path');
 
-const port = 3001;
+const port = 80;
 
 function getLineId(lineNo) {
     const lineType = lineNo[lineNo.length - 1];
